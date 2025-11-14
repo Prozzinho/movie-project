1488506a0d0feddc7f0a8cce86cf2daa5722c940
mithridatem <mathieumithridate@adrar-formation.com>
2025-11-14 14:49:19 +0100

    docs: add commentary index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'vendor/autoload.php';
 $url = parse_url($_SERVER["REQUEST_URI"]);
 
 $path = isset($url["path"]) ? $url["path"] : "/";
-
+//router
 switch ($path) {
     case '/':
         echo "Home";
